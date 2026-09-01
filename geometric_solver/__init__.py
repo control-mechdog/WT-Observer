a7da19cb84999c39a28e7343524c8f65766cd434
control-mechdog <2598764162@qq.com>
2026-09-01 10:49:43 +0800

    Add geometric solver package exports

---
+++ geometric_solver/__init__.py
@@ -0,0 +1,20 @@
+"""Reference geometric solver used by WT-Observer."""
+
+from .wt_pose_from_unit_centerlines import (
+    PoseDegeneracyError,
+    solve_pose_from_unit_centerlines,
+)
+from .wt_pose_to_blade_centerlines import (
+    add_independent_angular_noise,
+    orientation_class_from_relative_yaw,
+    project_blade_centerlines,
+)
+
+__all__ = [
+    "PoseDegeneracyError",
+    "add_independent_angular_noise",
+    "orientation_class_from_relative_yaw",
+    "project_blade_centerlines",
+    "solve_pose_from_unit_centerlines",
+]
+
